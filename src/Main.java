5b9d855afc1a59f9f59454c49c5ecfdc0f968b28
Сергей <s.v.yakovlev@yandex.ru>
2022-06-20 22:46:47 +0400

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
+
+// Задание №1
+        int[] arr = generateRandomArray();
+        int summ = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + summ + " рублей.");
 
     }
 }
